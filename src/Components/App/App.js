2ae79db94cdca046dee1bc11b87a56e80f576485
Boone Epstein <booneepstein@gmail.com>
2021-02-17 11:21:12 -0600

    Set up controlled form

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import apiCalls from '../../apiCalls';
 import ReservationCard from '../ReservationCard/ReservationCard'
+import ReservationForm from '../ReservationForm/ReservationForm'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      formName: '',
+      formDate: null,
+      formTime: null,
+      formGuests: null,
       reservations: []
     }
   }
@@ -16,12 +21,27 @@ class App extends Component {
       .then(response => this.setState({reservations: response}))
   }
 
+  updateForm = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  addRes = (event) => {
+    return true
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <ReservationForm
+            name={this.state.formName}
+            date={this.state.formDate}
+            time={this.state.formTime}
+            guests={this.state.formGuests}
+            updateForm={this.updateForm}
+            addRes={this.addRes}
+          />
         </div>
         <div className='resy-container'>
           {this.state.reservations.map(reservation =>
---
+++ src/Components/ReservationForm/ReservationForm.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const ReservationForm = ({name, date, time, guests, updateForm, addRes}) => {
+  return (
+    <>
+      <input
+        name='formName'
+        type='text'
+        value={name}
+        onChange={updateForm}
+      />
+      <input
+        name='formDate'
+        type='date'
+        value={date}
+        onChange={updateForm}
+      />
+      <input
+        name='formTime'
+        type='time'
+        value={time}
+        onChange={updateForm}
+      />
+      <input
+        name='formGuests'
+        type='number'
+        min='1'
+        value={guests}
+        onChange={updateForm}
+      />
+    </>
+  )
+}
+
+export default ReservationForm
