aa5839928582e653db3e5d11d79b8cec62ce3f2d
Boone Epstein <booneepstein@gmail.com>
2021-02-17 10:17:02 -0600

    Add constructor to app with reservations state

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      rservations: []
+    }
+  }
+  
   render() {
     return (
       <div className="App">
@@ -10,7 +17,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+
         </div>
       </div>
     )
