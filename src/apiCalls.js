020811a84169ee0baf3bc13b1da18978ef521e5a
Boone Epstein <booneepstein@gmail.com>
2021-02-17 12:21:41 -0600

    Add post api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,6 +2,16 @@ const apiCalls = {
   getReservations() {
     return fetch('http://localhost:3001/api/v1/reservations')
       .then(response => response.json())
+  },
+
+  postReservation(newRes) {
+    fetch('http://localhost:3001/api/v1/reservations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newRes)
+    })
   }
 }
 
